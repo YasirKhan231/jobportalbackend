190835758b126e6e341c1fc99fe7541d1878b4e0
vikaskirdoliya <134048517+vikaskirdoliya@users.noreply.github.com>
2025-06-28 12:13:01 +0530

    backend

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: ["http://localhost:5173", "https://job-portal-vikas.vercel.app"],
+  credentials: true,
+};
+
 
 app.use(cors(corsOptions));
 

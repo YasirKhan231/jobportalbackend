be6b8f771e83a7ca873217b58a7681eb96ad903d
Yasir khan <149812990+YasirKhan231@users.noreply.github.com>
2025-06-28 16:47:38 +0530

    Update indexjs cors all access

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://jobvista-vik.vercel.app",
+  origin: "*",
   credentials: true,
 };
 

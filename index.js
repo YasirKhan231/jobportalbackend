3fbe36cd1b5ca8198a66a1ff3004c394f0b87dbd
vikasonly <vikaskirdoliya2004@gmail.com>
2025-06-28 15:20:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://jobvista-vik.vercel.app"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(
+  cors({
+    origin: ["https://jobvista-vik.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 

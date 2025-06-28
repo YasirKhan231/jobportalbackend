0a43c3d84764592fb5b5d79febf29f0a6e83bb72
vikasonly <vikaskirdoliya2004@gmail.com>
2025-06-28 15:40:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://jobvista-vik.vercel.app/"
+  "https://jobvista-vik.vercel.app"
 ];
 
 const corsOptions = {
